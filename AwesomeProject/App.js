9347d8381132612cdd0ed9c76d1f8a8700455748
Meri <twinkling.star.m.t@gmail.com>
2019-05-13 23:53:33 +0900

    コーヒーのイメージを表示

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -21,6 +21,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     // aliginItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
 });
--- AwesomeProject/screens/Main.js
+++ AwesomeProject/screens/Main.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image} from 'react-native';
+import { StyleSheet, Text, View, Image, Button} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 70,
-    padding: 20,
+    marginTop: 20,
   },
   coffee: {
-    // backgroundColor: 'powderblue',
+    backgroundColor: 'powderblue',
     alignItems: 'center',
+    height:200,
+
   },
-  // coffee_quantity: {
+  coffee_img: {
+    marginTop: 100,
+    padding: 20,
+  }
+    // coffee_quantity: {
   //   color: '#3e2723',
   //   fontSize: 70,
   // }
@@ -21,7 +26,15 @@ export default class Main extends React.Component {
   render() {
     return(
       <View style= { styles.container }>
-          <Image style= { styles.coffee} source={ require('../assets/coffee.png')}></Image>
+        <View style= {styles.coffee}>
+          <Image style= { styles.coffee_img} source={ require('../assets/coffee.png')}></Image>
+          <Button
+  // onPress={ onPress }
+  title="Learn More"
+  color="#841584"
+  accessibilityLabel="Learn more about this purple button"
+/>
+        </View>
       </View>
     )
   }
